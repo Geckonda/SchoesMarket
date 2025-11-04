5d447e1ae6f9dc562808f15f601c1e613da82d3f
Geckonda <aquariusstd@yandex.ru>
2025-11-04 12:50:28 +0400

    feat(AppDbContext): добавление контекста + миграции

--- SchoesMarket.DAL/AppDbContext.cs
+++ SchoesMarket.DAL/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ShoesMarket.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace SchoesMarket.DAL
 {
-    public class AppDbContext 
+    public class AppDbContext : DbContext
     {
+        public DbSet<UserEntity> Users {  get; set; }
+        public DbSet<OrderEntity> Orders{  get; set; }
+        public DbSet<PickupPointEntity> PickupPoints {  get; set; }
+        public DbSet<ProductEntity> Products {  get; set; }
+        public DbSet<RoleEntity> Roles{  get; set; }
+        public DbSet<OrderDetailsEntity> OrdersDetailes{  get; set; }
+
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
     }
 }
